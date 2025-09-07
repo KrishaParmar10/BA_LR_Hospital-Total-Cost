418c6e158f79a6b7a42a5efc028eaf34e9a4e6e2
Krisha Parmar <parmarkrisha10@gmail.com>
2025-09-07 12:47:23 +0530

    Update adv_app.py

--- adv_app.py
+++ adv_app.py
@@ -18,6 +18,9 @@ COST_OF_IMPLANT = st.number_input('COST_OF_IMPLANT', min_value=0.0)
 # Make prediction 
 if st.button('Predict Hospital Total Cost'): 
     input_data = np.array([[TOTAL_AMOUNT_BILLED_TO_THE_PATIENT, CONCESSION, ACTUAL_RECEIVABLE_AMOUNT, TOTAL_LENGTH_OF_STAY, LENGTH_OF_STAY_ICU, LENGTH_OF_STAY_WARD, COST_OF_IMPLANT]]) 
+    st.write("Model expects features:", model.n_features_in_)
+    st.write("Input shape:", input_data.shape)
     prediction = model.predict(input_data)[0] 
 
     st.success(f'Predicted Hospital Cost: {prediction:.2f}')
+
